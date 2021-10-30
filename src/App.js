8caff645aa871033cbeb3a82019c52018b8d822d
KrishWu <66881117+TechnoKyle27@users.noreply.github.com>
2021-10-30 10:40:38 -0700

    Worked on adding images and tailwind CSS.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-blue-500">Charging Champions</h1>
+    <div className="bg-blue-100 h-full w-full">
+      <header className="flex justify-around m-3 border-2 b">
+        <img className="w-12" src="https://i.imgur.com/AkKEJZ1.png" alt="Charging Champion's Logo" />
+        <h1 className="text-blue-500 text-5xl text-center">Charging Champions</h1>
+      </header>
+      <img className="w-1/2" src="https://i.imgur.com/ArqgbAB.jpeg" alt="Chargine Champion's Team" />
     </div>
   );
 }
